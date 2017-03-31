53f48d5991af0a52d16de37fa0927992b3d58171
Chainatee Tanakulrungson <c.tanakul@gmail.com>
2017-03-30 22:19:11 -0500

    finish PS1.py

---
+++ HW1/PS1.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# DOCUMENTATION
+# =====================================
+# Class node attributes:
+# ----------------------------
+# children - dictionary containing the children where the key is child number (1,...,k) and the value is the actual node object
+# if node has no children, self.children = None
+# value - value at the node
+#
+#
+# The values for bfs should be returned as simply a string of value space value space value. For example if the tree looks like the following:
+#     5
+#   2   3
+#
+# The tree data structure is a node with value 5, with a dictionary of children {1: b, 2: c} where b is a node with value 2 and c is a node with value 3.  Both b and c have children of None.
+# The bfs traversal of the above tree should return the string '5 2 3'
+class Node:
+	def __init__(self):
+		self.value = None
+		self.children = None
+
+	def get_value(self):
+		
+		'''
+		given a node, will return the value at this node
+		'''
+                return self.value
+
+	def get_children(self):
+		
+		'''
+		given a node, will return the children of this node
+		'''
+                if bool(self.children):
+                    ret_str = ''
+                    for key in range(len(self.children)):
+                        ret_str = ret_str + str(self.children[key + 1]) + ' '
+                    return ret_str
+                else:
+                    return ''
+
+def breadth_first_search(root):
+	
+	'''
+	given the root node, will complete a breadth-first-search on the tree, returning the value of each node in the correct order
+	'''
+        ret_str = ''
+        ret_str += str(root.get_value()) + ' '
+        if bool(root.children):
+            for key in range(len(root.children)):
+                ret_str += breadth_first_search(root.children[key + 1]) + ' '
+        return ret_str
+
+def tester():
+	a = Node()
+	a.value = 5
+        b = Node()
+	b.value = 7
+	a.children = {1: b}
+	print str(a.get_value()) + ' should be 5.'
+	print str(a.get_children()) + ' should be {1: ' + str(b) + '}.'
+	print str(breadth_first_search(a)) + ' should be 5 7.'
+
+
+
+def main():
+    tester()
+
+
+if __name__=='__main__':
+    main()
